765ab26237305f681026b69cda2ee67654429132
Saurav Singh <sauravcloud26@gmail.com>
2026-05-22 18:55:21 +0530

    deep link added

--- App.tsx
+++ App.tsx
@@ -4,14 +4,32 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "./src/context/AuthContext";
 import AppStack from "./src/navigators/stack/DynamicStackNavigator";
 import { CardProvider } from "./src/context/CartContext";
+import { LinkingOptions } from "@react-navigation/native";
+
+const linking: LinkingOptions<any> = {
+  prefixes: ["foodapp://"],
+
+  config: {
+    screens: {
+      LeftDrawer: {
+        path: "",
+
+        screens: {
+          RestaurantDetail: "restaurant/:restaurantId",
+        },
+      },
+    },
+  },
+};
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+      <NavigationContainer linking={linking}>
         <AuthProvider>
           <CardProvider>
             <StatusBar style="light" />
+
             <AppStack />
           </CardProvider>
         </AuthProvider>
